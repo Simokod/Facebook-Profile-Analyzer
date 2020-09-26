cd710e63ec4e7f949193bd134b2a7e2cf19c583a
t-yucohe <t-yucohe@microsoft.com>
2020-09-26 20:07:37 +0300

    added client side + html templates

--- server.py
+++ server.py
@@ -1,14 +1,21 @@
 import managerV2
-from flask import Flask, request
+from flask import Flask, request, render_template
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/output")
 def output():
-	return "Hello World!"
+	return render_template('scan_result.html', scan_result="Hello World!")
+
+@app.route('/')
+def home():
+   return render_template('home.html')
 
 @app.route("/analyze/<email>/<password>/<int:mod>/<int:scrape_mod>", methods=['GET'])
 def analyze(email, password, mod, scrape_mod):
-	return managerV2.scrape_and_analyze(email, password, mod, scrape_mod)
+	scan_result = managerV2.scrape_and_analyze(email, password, mod, scrape_mod)
+	return render_template('scan_result.html', scan_result=scan_result)
 
 if __name__ == "__main__":
 	app.run()
